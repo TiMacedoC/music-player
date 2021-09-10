29fff86e160ef0e8870af762b336f0a9308a81f2
Tiago Macedo <tiagomacedo99@hotmail.com>
2021-09-10 19:22:31 -0300

    Criado as constantes que vou precisar

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
+let currentMusic = 0;
+
+const music = document.querySelector('#audio');
+
+const songName = document.querySelector('.song-name');
+const artistName = document.querySelector('.artist-name');
+const diskImage = document.querySelector('.disk');
+const seekBar = document.querySelector('.seek-bar');
+const currentTime = document.querySelector('.current-time');
+const songDuration = document.querySelector('.song-duration');
+const backwardBtn = document.querySelector('.previous-song');
 const playButton = document.querySelector('.play-btn');
-const diskImage = document.querySelector('.disk')
+const forwardBtn = document.querySelector('.next-song');
+
+
+
+
+
+
 
 
 playButton.addEventListener("click", function playPause() {
