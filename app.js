32afa78508502d349f94fe02b3b0e1fe71227dd7
Tiago Macedo <tiagomacedo99@hotmail.com>
2021-09-10 22:05:16 -0300

    ajustando alguns detalhes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let currentMusic = 0;
+var isPlaying = false;
 
 const music = document.querySelector('#audio');
 
@@ -13,15 +14,104 @@ const playButton = document.querySelector('.play-btn');
 const forwardBtn = document.querySelector('.next-song');
 
 
-
-
-
-
-
-
-playButton.addEventListener("click", function playPause() {
+// Play button
+function playPause() {
     playButton.classList.toggle("pause");
-
     diskImage.classList.toggle("pause");
 
+    playingCheck() ? music.pause() : music.play();
+
+
+}
+
+
+function playingCheck() {
+
+    music.onplaying = () => {
+        isPlaying = true;
+    }
+    music.onpause = () => {
+        isPlaying = false;
+    }
+
+    return isPlaying;
+}
+
+
+//Set music
+function setMusic(musicIndex) {
+    seekBar.value = 0;
+    let song = songs[musicIndex];
+    currentMusic = musicIndex;
+    music.src = song.path;
+
+    songName.innerHTML = song.name;
+    artistName.innerHTML = song.artist;
+    diskImage.style.backgroundImage = `url('${song.cover}')`
+
+    currentTime.innerHTML = '00:00'
+
+    setTimeout(() => {
+        seekBar.max = music.duration;
+        songDuration.innerHTML = formatTime(music.duration);
+    }, 200);
+}
+
+setMusic(0)
+
+// Seek Bar
+setInterval(() => {
+    currentTime.innerHTML = formatTime(music.currentTime);
+    seekBar.value = music.currentTime;
+}, 500);
+
+seekBar.addEventListener('change', () => {
+    music.currentTime = seekBar.value;
+    console.log(seekBar.value);
+})
+
+// Next and previous song
+backwardBtn.addEventListener('click', () => {
+    currentMusic == 0 ? setMusic(songs.length - 1) : setMusic(currentMusic - 1);
+    playingCheck() ? music.play() : music.pause();
+
 });
+
+forwardBtn.addEventListener('click', () => {
+    currentMusic == (songs.length - 1) ? setMusic(0) : setMusic(currentMusic + 1);
+    playingCheck() ? music.play() : music.pause();
+
+});
+
+//Auto next
+setInterval(function autoNext() {
+    if (music.currentTime >= (music.duration - 0.9)) {
+        console.log("acabou", music.currentTime, music.duration)
+        forwardBtn.click()
+    }
+}, 500);
+
+// Include media keys control
+navigator.mediaSession.setActionHandler('play', function () { playButton.click() });
+navigator.mediaSession.setActionHandler('pause', function () { playButton.click() });
+navigator.mediaSession.setActionHandler('previoustrack', function () { backwardBtn.click() });
+navigator.mediaSession.setActionHandler('nexttrack', function () { forwardBtn.click() });
+
+
+// Format Time
+function formatTime(time) {
+    let min = Math.floor(time / 60);
+    if (min < 10) {
+        min = `0${min}`;
+    }
+
+    let sec = Math.floor(time % 60);
+    if (sec < 10) {
+        sec = `0${sec}`;
+    }
+
+    return (`${min}:${sec}`);
+}
+
+
+
--- data.js
+++ data.js
@@ -1,32 +1,32 @@
 let songs = [
     {
-        name: 'song 1',
-        path: 'musics/Song 1.mp3',
+        name: 'song one',
+        path: 'assets/musics/Song 1.mp3',
         artist: 'artist 1',
-        cover: 'images/cover 1.png'
+        cover: 'assets/images/cover 1.png'
     },
     {
-        name: 'song 2',
-        path: 'musics/Song 2.mp3',
+        name: 'song two',
+        path: 'assets/musics/Song 2.mp3',
         artist: 'artist 2',
-        cover: 'images/cover 2.png'
+        cover: 'assets/images/cover 2.png'
     },
     {
-        name: 'song 3',
-        path: 'musics/Song 3.mp3',
+        name: 'song three',
+        path: 'assets/musics/Song 3.mp3',
         artist: 'artist 3',
-        cover: 'images/cover 3.png'
+        cover: 'assets/images/cover 3.png'
     },
     {
-        name: 'song 4',
-        path: 'musics/Song 4.mp3',
+        name: 'song four',
+        path: 'assets/musics/Song 4.mp3',
         artist: 'artist 4',
-        cover: 'images/cover 4.png'
+        cover: 'assets/images/cover 4.png'
     },
     {
-        name: 'song 5',
-        path: 'musics/Song 5.mp3',
+        name: 'song five',
+        path: 'assets/musics/Song 5.mp3',
         artist: 'artist 5',
-        cover: 'images/cover 5.png'
+        cover: 'assets/images/cover 5.png'
     },
 ]
